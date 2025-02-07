8f7468d1297c48f2f6002d246868f69f05c21e7a
alinazarif <alinazarif06@gmail.com>
2025-02-07 22:46:12 +0500

    Add files via upload

---
+++ Resturant-E_Commerce-main/Resturant-E_Commerce-main/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Slider from "@/components/slider"
+import Featured from "@/components/featured"
+import Offer from "@/components/offer"
+
+export default function Home() {
+  return (
+  <div>
+    <Slider/>
+    <Featured/>
+    <Offer/>
+  </div>
+  )
+}
